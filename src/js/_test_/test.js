b9c2a33da5c4da7d7fbd7476cfb466e3460df2ad
Родионов Алексей Игоревич <rodionov_a@tii.ru>
2023-01-29 21:16:50 +0300

    test 1

---
+++ src/js/_test_/test.js
@@ -0,0 +1,10 @@
+import lifeIndication from "../index";
+
+test.each([
+    ["Маг healthy", { name: "Маг", health: 90 }],
+    ["Маг wounded", { name: "Маг", health: 35 }],
+    ["Маг critical", { name: "Маг", health: 10 }],
+])("Проверка работы функции c показателем жизни %s", (expected, obj) => {
+    const result = lifeIndication(obj);
+    expect(result).toBe(expected);
+});
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+export default function lifeIndication({ name, health }) {
+    switch (true) {
+        case health >= 50:
+            return `${name} healthy`;
+        case health >= 15 && health < 50:
+            return `${name} wounded`;
+        default:
+            return `${name} critical`;
+    }
+}
+
+lifeIndication({ name: "Маг", health: 90 });
